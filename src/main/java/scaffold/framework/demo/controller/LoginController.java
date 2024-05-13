c5c503be33cccadffb27aa49450de466674a1395
dylancodeverse <ratianarivodylan@gmail.com>
2024-05-13 14:04:48 +0300

    ok

--- src/main/java/scaffold/framework/demo/controller/LoginController.java
+++ src/main/java/scaffold/framework/demo/controller/LoginController.java
@@ -1,7 +1,17 @@
 package scaffold.framework.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import scaffold.framework.demo.FormHelper.ValidationHelper;
+import scaffold.framework.demo.config.ValidatorConfig;
+import scaffold.framework.demo.models.Utilisateur;
 
 @Controller
 public class LoginController {
@@ -9,6 +19,38 @@ public class LoginController {
     @GetMapping("/login")
     public String pageLogin() {
         return "/pages/authentification/login";
+    }
+
+    @PostMapping("/login")
+    public String postMethodName(HttpServletRequest request, String numero, Model model) throws Exception {
+        Utilisateur utilisateur = new Utilisateur();
+        ValidationHelper validator = ValidatorConfig.getValidationHelper();
+        validator.validate(utilisateur, model, "numero", numero);
+
+        if (!validator.hasError()) {
+            // jerevo ny users misy an iny num iny
+            Utilisateur[] users = utilisateur.selectWhere(" numero = '" + numero + "'");
+            Integer a = users.length;
+            if (a == 0) {
+                try {
+                    // raha tsy hita de insereo
+                    utilisateur.setHierarchie(0);
+                    utilisateur.insert();
+                } catch (Exception e) {
+                    // raha sanatria tsy mety inserer-na dia miverena amn page login miaraka amn
+                    // erreur
+                    model.addAttribute("error", e.getMessage());
+                    return "/pages/authentification/login";
+                }
+            } else {
+                utilisateur.setId(users[0].getId());
+            }
+            HttpSession session = request.getSession(true);
+            session.setAttribute("id", utilisateur.getId());
+        }
+
+        return "/pages/authentification/login";
+
     }
 
 }
